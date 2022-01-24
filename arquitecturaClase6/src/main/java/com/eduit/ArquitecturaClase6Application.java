d2d96543944914dff05e9b74fe9d2bf80cff4078
Rafael Ibarra <rafael.dante@gmail.com>
2022-01-24 20:32:25 -0300

    ejemplo

---
+++ arquitecturaClase6/src/main/java/com/eduit/ArquitecturaClase6Application.java
@@ -0,0 +1,13 @@
+package com.eduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturaClase6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquitecturaClase6Application.class, args);
+	}
+
+}
